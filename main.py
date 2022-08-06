594c587112cc887c169a3ceb9908c2a681984986
dkohn42 <dkohn@pdx.edu>
2022-08-06 00:17:15 -0700

    Final

--- main.py
+++ main.py
@@ -7,33 +7,58 @@ datafile = './545_cluster_dataset programming 3.csv'
 X = 0
 Y = 1
 DIM = 2
-CLUSTERS = 5
+CLUSTERS = 15
 CLUSTER = []
-RUNS = 10
-MAX_ITERATIONS = 100
+RUNS = 1
+MAX_ITERATIONS = 50
 COORD = {X : 'X', Y : 'Y'}
 SCALE = 1
 EQUILIBRIUM = 0.02
-FULLSCREEN = False
-OUTPUT = True
+FULLSCREEN = True
+OUTPUT = False
+FONTSIZE = 10
+if CLUSTERS > 12:
+    FONTSIZE -= int((CLUSTERS - 9)/ 3)
+    if FONTSIZE < 5:
+        FONTSIZE = 5
 
 def plot_scatter(x, y, centroid):
     # Plot cluster points
     plt.scatter(x, y, s=(10 * SCALE))
     # Plot cluster centroid
     plt.scatter(centroid[X], centroid[Y], color = 'black', s=(40 * SCALE))
-
+    
 def calc_mse(clusters, centroid):
-    mses = []
+    mse = []
+    # Calculate Mean Squares Error for each cluster
     for c in range(CLUSTERS):
-        mses.append(np.subtract(clusters[c], centroid[c]))
-        mses[c] = mses[c].pow(2)
-        mses[c] = mses[c].sum(axis=1)
-        mses[c] = mses[c].sum() / len(mses[c])
-    return mses
+        # Subtract X and Y values of the centroid from every X and Y value in the cluster
+        mse.append(np.subtract(clusters[c], centroid[c]))
+        # Square all the X and Y values 
+        mse[c] = mse[c].pow(2)
+        # Sum the X and Y values for each data point in the cluster (distance)
+        mse[c] = mse[c].sum(axis=1)
+        # Average the distances from the centroid (MSE)
+        mse[c] = np.sum(mse[c]) / len(mse[c])
+    return mse
 
-def calc_avg_mse(mses):
-    return sum(mses)/len(mses)
+def calc_avg_mse(mse):
+    # Find the average MSE
+    return sum(mse)/len(mse)
+
+def calc_mss(centroid):
+    matrix = []
+    # Create a data frame with the centroid data
+    df = pd.DataFrame(data = centroid)
+    for d in range(DIM):
+        matrix.append(np.array(pd.DataFrame(np.array(df[d]))))
+        vector = np.transpose(matrix[d])
+        matrix[d] = np.tile(matrix[d], (1, CLUSTERS))
+        matrix[d] = np.subtract(matrix[d], vector)
+        matrix[d] = np.power(matrix[d], 2)
+    sum = np.sum(matrix) / 2
+    mss = (sum) / (CLUSTERS * (CLUSTERS-1) / 2)
+    return mss
 
 # Load data 
 with open(datafile, newline='') as csvfile:
@@ -50,121 +75,7 @@ for run in range(RUNS):
     # Randomly select centroids 
     centroid = [[0,0] for i in range(CLUSTERS)]
     for c in range(CLUSTERS):
-        index = random.randint(0,N)
-        centroid[c][X] = data[COORD[X]][index]
-        centroid[c][Y] = data[COORD[Y]][index]
-
-    done = False
-    iteration = 1
-    while (done == False and iteration <= MAX_ITERATIONS):
-        print(f'Iteration: {iteration}')
-        done = True
-        distances = pd.DataFrame()
-        # Calculate distances from each point to every centroid and store in 'distances'
-        for c in range(CLUSTERS):
-            distances_tmp = pd.DataFrame(np.subtract(data, centroid[c]))
-            distances_tmp = distances_tmp.pow(2)
-            distances[c] = distances_tmp.sum(axis=1)
-        # Find the closest centroid for each point, store result in 'closest_centroid'
-        closest_centroid = distances.idxmin(axis=1)
-    # Calc new centroids as mean of all X and Y values in the cluster
-
-        cluster_assignment = []
-        # Create cluster dataframes to store X and Y coords
-        for cluster in range (CLUSTERS):
-            cluster_assignment.append(pd.DataFrame(columns = ['X', 'Y']))
-        # Sort data into their clusters
-        for index in range(N-1):
-            length = len(cluster_assignment[closest_centroid[index]][COORD[X]].index)
-            cluster_assignment[closest_centroid[index]].loc[length] = data.iloc[index]
-
-        # Calculate Mean Square Error and Average MSE
-        mses = calc_mse(cluster_assignment, centroid)
-        avg_mse = calc_avg_mse(mses)
-
-        plt.clf()
-        for cluster in range(CLUSTERS):
-            x = cluster_assignment[cluster][COORD[X]]
-            y = cluster_assignment[cluster][COORD[Y]]
-            # Plot cluster points
-            plot_scatter(x,y,centroid[cluster])
-            # Calc new centroids as mean of all X and Y values in the cluster
-            new_centroid_x = float(x.sum())/len(x.index)
-            new_centroid_y = float(y.sum())/len(y.index) 
-            # Check - Change in X and Y value < EQUILIBRIUM? 'done' when all centroids X & Y DELTA < EQUILIBRIUM
-            if (np.abs((centroid[cluster][X] / new_centroid_x) - 1) > EQUILIBRIUM): 
-                done = False
-            if (np.abs((centroid[cluster][Y] / new_centroid_y) - 1) > EQUILIBRIUM): 
-                done = False
-            # Store new centroids
-            centroid[cluster][X]= new_centroid_x
-            centroid[cluster][Y]= new_centroid_y
-        plt.text(-5,-5.2, f'AVG MSE: {round(avg_mse, 4)}')
-        plt.pause(0.01)
-        if(OUTPUT == True):
-            plt.savefig(f'output-c{CLUSTERS}-r{run}-i{iteration}.jpg')
-        iteration += 1
-    print("FIN")
-#    plt.show()
-
-aimport random
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-datafile = './545_cluster_dataset programming 3.csv'
-X = 0
-Y = 1
-DIM = 2
-CLUSTERS = 9
-CLUSTER = []
-RUNS = 1
-MAX_ITERATIONS = 10
-COORD = {X : 'X', Y : 'Y'}
-SCALE = 1
-EQUILIBRIUM = 0.02
-FULLSCREEN = False
-OUTPUT = False
-
-def plot_scatter(x, y, centroid, ax):
-    # Plot cluster points
-    plt.scatter(x, y, s=(10 * SCALE))
-    # Plot cluster centroid
-    plt.scatter(centroid[X], centroid[Y], color = 'black', s=(40 * SCALE))
-    
-#def plot_info(fig, ax, avg_mse):
-    
-
-def calc_mse(clusters, centroid):
-    mses = []
-    for c in range(CLUSTERS):
-        mses.append(np.subtract(clusters[c], centroid[c]))
-        mses[c] = mses[c].pow(2)
-        mses[c] = mses[c].sum(axis=1)
-        mses[c] = mses[c].sum() / len(mses[c])
-    return mses
-
-def calc_avg_mse(mses):
-    return sum(mses)/len(mses)
-
-# Load data 
-with open(datafile, newline='') as csvfile:
-    data = pd.read_csv(csvfile, sep='  ', engine='python')
-N = len(data)
-
-# Display full screen
-if (FULLSCREEN == True):
-    manager = plt.get_current_fig_manager()
-    manager.full_screen_toggle()
-    SCALE = 5
-
-fig,ax = plt.subplots()
-
-for run in range(RUNS):
-    # Randomly select centroids 
-    centroid = [[0,0] for i in range(CLUSTERS)]
-    for c in range(CLUSTERS):
-        index = random.randint(0,N)
+        index = random.randint(0,N-1)
         centroid[c][X] = data[COORD[X]][index]
         centroid[c][Y] = data[COORD[Y]][index]
 
@@ -197,16 +108,17 @@ for run in range(RUNS):
             # Increment the size of the current cluster
             cluster_size[cluster] += 1
 
-        # Calculate Mean Square Error and Average MSE
-        mses = calc_mse(cluster_assignment, centroid)
-        avg_mse = calc_avg_mse(mses)
+        # Calculate Mean Square Error,Average MSE,Mean Square Separation, and Entropy
+        mse = calc_mse(cluster_assignment, centroid)
+        avg_mse = calc_avg_mse(mse)
+        mss = calc_mss(centroid)
 
         plt.clf()
         for cluster in range(CLUSTERS):
             x = cluster_assignment[cluster][COORD[X]]
             y = cluster_assignment[cluster][COORD[Y]]
             # Plot cluster points
-            plot_scatter(x,y,centroid[cluster], ax)
+            plot_scatter(x,y,centroid[cluster])
             # Calc new centroids as mean of all X and Y values in the cluster
             new_centroid_x = float(x.sum())/len(x.index)
             new_centroid_y = float(y.sum())/len(y.index) 
@@ -218,11 +130,17 @@ for run in range(RUNS):
             # Store new centroids
             centroid[cluster][X]= new_centroid_x
             centroid[cluster][Y]= new_centroid_y
-        plt.text(-5,-5.2, f'AVG MSE: {round(avg_mse, 4)}')
+        # Display average MSE 
+        plt.text(-5,-5.2, f'Iteration: {iteration}   AVG MSE: {round(avg_mse, 3)}   MSS: {round(mss, 3)}')
+        # Display cluster MSEs
+        for cluster in range(CLUSTERS):
+            x_pos = -5 + (.17*FONTSIZE * int(cluster / 3))
+            y_pos = 5.2 - (0.3 * (cluster % 3))
+            plt.text(x_pos, y_pos,f'MSE {cluster}: {round(mse[cluster],SCALE+1)}', fontsize=FONTSIZE+SCALE)
         plt.pause(0.01)
         if(OUTPUT == True):
-            plt.savefig(f'output-c{CLUSTERS}-r{run}-i{iteration}.jpg')
+            plt.savefig(f'output-c{CLUSTERS}-r{run}-i{iteration}.jpg', fontsize=9+SCALE)
         iteration += 1
     print("FIN")
-#    plt.show()
+    plt.show()
 
